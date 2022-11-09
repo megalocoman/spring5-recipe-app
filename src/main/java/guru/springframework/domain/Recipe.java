ec970b96e4f16c0cfc8c5f0c244444766224d3d0
megalocoman <cfjofre@uc.cl>
2022-11-08 21:15:21 -0300

    - many to many - repositories

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -16,6 +16,8 @@ public class Recipe {
     private String source;
     private String url;
     private String direction;
+    @Enumerated(value= EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredient;
@@ -23,6 +25,11 @@ public class Recipe {
     private  Byte[] images;
    @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+    @ManyToMany()
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name="recipe_id"),
+            inverseJoinColumns = @JoinColumn(name="category_id"))
+   private Set<Category> categories;
 
     public String getDescription() {
         return description;
@@ -94,5 +101,29 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Set<Ingredient> ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
